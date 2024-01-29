0c31c18b4de1bad5080dea3f5d452d653452bb23
David Igbayilola <43897620+SoftwareSultanDavid@users.noreply.github.com>
2024-01-29 14:14:40 +0100

    Added download function, favicon, validation on input

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import QRCode from 'qrcode';
-
-
+import favicon from'serve-favicon';
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
@@ -12,25 +14,51 @@ const port = 5000;
 // middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(favicon(__dirname + "/public/img/favicon.ico"));
+
+
 
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', { generatedQRcode: '' });;
+  res.render('index.ejs', { generatedQRcode: '', download: ''});;
 });
 
 app.post('/qrcode', async(req, res) => {
   try {
-    if(!req.body.url){
-      res.redirect('/');
-    }
-    const generatedQRcode = await QRCode.toDataURL(req.body.url);
-    res.render('index.ejs', { generatedQRcode });
+    // generate qrcode data
+    const generatedQRcode = await QRCode.toDataURL(req.body.url, {
+      scale: 8,
+      margin: 4,
+    });
+
+
+    // save qrcode to file for download feature
+      QRCode.toFile('public/img/data/qrcode.png', req.body.url, function (err) {
+      // new qrcode overwrites the old one
+      if (err) throw err;
+      console.log('File created!');
+    });``
+
+
+    // File overwrite and being passed 
+    res.render('index.ejs', { generatedQRcode, download: '/img/data/qrcode.png'});
   } catch (err) {
     throw err;
   }
 });
 
+// Error handling middleware
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.redirect('/');
+});
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,17 @@
 // submit button
 const submitButton = document.querySelector("#submit");
+const downloadButton = document.querySelector("#download");
+const inputTextArea = document.querySelector("#url");
 
-
+// downloadButton.classList.add("disabled");
 
 submitButton.addEventListener("click", function(e) {
 
-  // e.preventDefault();
+
+  
+
+  // overide the download disabled buttton
+  // downloadButton.classList.remove("disabled");
 
 
 });
